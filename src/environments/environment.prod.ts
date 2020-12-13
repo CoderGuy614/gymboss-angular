f214fd49dd0d3c0243ac98c8a3f59b4cf7179b38
CoderGuy614 <jlutz.110@gmail.com>
2020-12-13 16:24:06 -0500

    Initial Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAeZj9Fqcpp1w6J9w25FU6jDCfK_t4MNYw',
+    authDomain: 'gymboss-fb0f3.firebaseapp.com',
+    projectId: 'gymboss-fb0f3',
+    storageBucket: 'gymboss-fb0f3.appspot.com',
+    messagingSenderId: '665412064037',
+    appId: '1:665412064037:web:8b60759d0f8fc7333740cc',
+    measurementId: 'G-8SPQDHMV8P',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAeZj9Fqcpp1w6J9w25FU6jDCfK_t4MNYw',
+    authDomain: 'gymboss-fb0f3.firebaseapp.com',
+    projectId: 'gymboss-fb0f3',
+    storageBucket: 'gymboss-fb0f3.appspot.com',
+    messagingSenderId: '665412064037',
+    appId: '1:665412064037:web:8b60759d0f8fc7333740cc',
+    measurementId: 'G-8SPQDHMV8P',
+  },
 };
 
 /*
